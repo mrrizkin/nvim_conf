f187dd392216cab92ecd5d5c84c6095e9b655114
nugrhrizki <nugraha07rizki@gmail.com>
2021-08-29 06:53:00 +0700

    why sumneko_lua always ask for setup LOVE?

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,5 +1,4 @@
 local nvim_lsp = require("lspconfig")
-require("packer").loader("coq_nvim coq.artifacts")
 
 local function on_attach(client, bufnr)
     local function buf_set_keymap(...)
@@ -17,11 +16,8 @@ local function on_attach(client, bufnr)
     buf_set_keymap('n', 'gD',         [[<Cmd>lua vim.lsp.buf.declaration()<CR>]], opts)
     buf_set_keymap('n', 'gd',         [[<Cmd>lua vim.lsp.buf.definition()<CR>]], opts)
     buf_set_keymap('n', 'K',          [[<Cmd>Lspsaga hover_doc<CR>]], opts)
-    buf_set_keymap('n', '<C-f>',      [[<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>]], opts)
-    buf_set_keymap('n', '<C-b>',      [[<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>]], opts)
     buf_set_keymap('n', 'gi',         [[<cmd>lua vim.lsp.buf.implementation()<CR>]], opts)
     buf_set_keymap('i', '<C-k>',      [[<cmd>Lspsaga signature_help<CR>]], opts)
-    buf_set_keymap('n', '<C-k>',      [[<cmd>Lspsaga signature_help<CR>]], opts)
     buf_set_keymap('n', '<leader>wa', [[<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>]], opts)
     buf_set_keymap('n', '<leader>wr', [[<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>]], opts)
     buf_set_keymap('n', '<leader>wl', [[<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>]], opts)
@@ -35,7 +31,7 @@ local function on_attach(client, bufnr)
     buf_set_keymap('n', ']d',         [[<cmd>lua vim.lsp.diagnostic.goto_next()<CR>]], opts)
     buf_set_keymap('n', '<leader>q',  [[<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>]], opts)
     buf_set_keymap('n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
-    vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+    vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting_seq_sync()' ]]
 
     if client.resolved_capabilities.document_formatting then
         vim.api.nvim_command([[augroup Formatter]])
@@ -48,42 +44,38 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local servers = { 'bashls', 'html', 'cssls', 'vimls', 'elmls', 'tsserver', 'rust_analyzer' }
+local servers = { 'bashls', 'clangd', 'vimls', 'elmls', 'rust_analyzer' }
 for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup(
-        require("coq")().lsp_ensure_capabilities({
-            on_attach = on_attach,
-            capabilities = capabilities,
-            flags = { debounce_text_changes = 150 },
-        })
-    )
+    nvim_lsp[lsp].setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        flags = { debounce_text_changes = 150 },
+    })
 end
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-nvim_lsp.sumneko_lua.setup(
-    require("coq")().lsp_ensure_capabilities({
-        cmd = { "lua-language-server" },
-        on_attach = on_attach,
-        capabilities = capabilities,
-        root_dir = vim.loop.cwd,
-        flags = { debounce_text_changes = 150 },
-        settings = {
-            Lua = {
-                runtime = {
-                    version = "LuaJIT",
-                    path = runtime_path,
-                },
-                diagnostics = { globals = { "vim" } },
-                workspace = {
-                    library = vim.api.nvim_get_runtime_file('', true),
-                    maxPreload = 100000,
-                    preloadFileSize = 10000,
-                },
-                telemetry = { enable = false },
-            },
-        },
-    })
-)
+-- nvim_lsp.sumneko_lua.setup({
+--     cmd = { "lua-language-server" },
+--     on_attach = on_attach,
+--     capabilities = capabilities,
+--     root_dir = vim.loop.cwd,
+--     flags = { debounce_text_changes = 150 },
+--     settings = {
+--         Lua = {
+--             runtime = {
+--                 version = "LuaJIT",
+--                 path = runtime_path,
+--             },
+--             diagnostics = { globals = { "vim" } },
+--             workspace = {
+--                 library = vim.api.nvim_get_runtime_file('', true),
+--                 maxPreload = 100000,
+--                 preloadFileSize = 10000,
+--             },
+--             telemetry = { enable = false },
+--         },
+--     },
+-- })
