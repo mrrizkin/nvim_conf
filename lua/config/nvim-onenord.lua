1c441dafd91e1851dbc310d73ab21c6879997311
nugrhrizki <nugraha07rizki@gmail.com>
2022-11-02 08:09:55 +0700

    update: using custom poimandres colorscheme

---
+++ lua/config/nvim-onenord.lua
@@ -0,0 +1,23 @@
+-- require("onenord").setup({
+-- 	theme = "dark",
+-- 	borders = true, -- Split window borders
+-- 	fade_nc = false, -- Fade non-current windows, making them more distinguishable
+-- 	-- Style that is applied to various groups: see `highlight-args` for options
+-- 	styles = {
+-- 		comments = "italic",
+-- 		strings = "NONE",
+-- 		keywords = "NONE",
+-- 		functions = "NONE",
+-- 		variables = "NONE",
+-- 		diagnostics = "undercurl",
+-- 	},
+-- 	disable = {
+-- 		background = false, -- Disable setting the background color
+-- 		cursorline = false, -- Disable the cursorline
+-- 		eob_lines = true, -- Hide the end-of-buffer lines
+-- 	},
+-- 	-- Inverse highlight for different groups
+-- 	inverse = {
+-- 		match_paren = false,
+-- 	},
+-- })
