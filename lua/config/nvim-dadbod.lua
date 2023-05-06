b51319d46e85c37029d008198163647d35aeacda
nugrhrizki <nugraha07rizki@gmail.com>
2023-05-07 03:02:07 +0700

    update: fix db ui icons

--- lua/config/nvim-dadbod.lua
+++ lua/config/nvim-dadbod.lua
@@ -3,3 +3,30 @@ vim.g.db_ui_save_location = "~/.config/nvim/db_ui"
 vim.g.db_ui_use_nerd_fonts = 1
 vim.g.db_ui_win_position = "right"
 
+vim.g.db_ui_icons = {
+	expanded = {
+		db = "▾ 󰆼",
+		buffers = "▾ ",
+		saved_queries = "▾ ",
+		schemas = "▾ ",
+		schema = "▾ 󰙅",
+		tables = "▾ 󰓰",
+		table = "▾ ",
+	},
+	collapsed = {
+		db = "▸ 󰆼",
+		buffers = "▸ ",
+		saved_queries = "▸ ",
+		schemas = "▸ ",
+		schema = "▸ 󰙅",
+		tables = "▸ 󰓰",
+		table = "▸ ",
+	},
+	saved_query = "",
+	new_query = "󰓰",
+	tables = "󰓫",
+	buffers = "󰆼",
+	add_connection = "󱘖",
+	connection_ok = "✓",
+	connection_error = "✕",
+}
